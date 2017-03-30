9828aaed3771292aa113ae40474aeed2c42d82be
Bohdan Samusko <samusko@steelkiwi.com>
2017-03-30 13:48:43 +0300

    configure MainActivity for MVVM

--- app/src/main/java/moviefinder/samusko/com/androidmoviefindermvvm/ui/main/MainActivity.java
+++ app/src/main/java/moviefinder/samusko/com/androidmoviefindermvvm/ui/main/MainActivity.java
@@ -1,15 +1,35 @@
 package moviefinder.samusko.com.androidmoviefindermvvm.ui.main;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import moviefinder.samusko.com.androidmoviefindermvvm.BR;
 import moviefinder.samusko.com.androidmoviefindermvvm.R;
+import moviefinder.samusko.com.androidmoviefindermvvm.base.mvvm.activities.BindingActivity;
+import moviefinder.samusko.com.androidmoviefindermvvm.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BindingActivity<ActivityMainBinding, MainActivityViewModel> {
+
+    private MainActivityViewModel mainActivityViewModel;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public MainActivityViewModel onCreate() {
+        mainActivityViewModel = new MainActivityViewModel(this);
+        return mainActivityViewModel;
+    }
+
+    @Override
+    public int getVariable() {
+        return BR.viewModel;
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_main;
     }
 }
